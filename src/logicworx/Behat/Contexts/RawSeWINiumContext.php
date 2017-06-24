24e13f9ee422078035d1c0fd13d409bd99954d38
mark <mark@marescal.com>
2017-06-24 22:27:30 +0100

    remapped Path

--- src/logicworx/Behat/Contexts/RawSeWINiumContext.php
+++ src/logicworx/Behat/Contexts/RawSeWINiumContext.php
@@ -18,7 +18,7 @@ class RawSeWINiumContext implements seWINiumAwareContext
     
     //Read Config file.
 
-    $configFile="/sewinium.json";
+    $configFile=__DIR__.'/../../../../../../../sewinium.json";
 
     $fHandle = fopen($configFile, "r") or die("Unable to open file!");
     $this->cfgJson = fread($fHandle,filesize($configFile));
