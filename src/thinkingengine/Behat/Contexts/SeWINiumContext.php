59011a2668621e598429da2cb24a0421affa471f
mark <mark@marescal.com>
2017-07-02 11:02:02 +0100

    Added context functions for finding and selecting windows.

--- src/thinkingengine/Behat/Contexts/SeWINiumContext.php
+++ src/thinkingengine/Behat/Contexts/SeWINiumContext.php
@@ -59,8 +59,11 @@ class SeWINiumContext extends RawSeWINiumContext implements TranslatableContext
        return;
     }
 
+    /* -------------------------------------- Window by title -----------------------------
+
+
      /**
-     * Window Exists
+     * Find a window with title
      * Example: Given I can find a window titled "winname"
      *
      * @Given /^(?:|I )can find a window titled "([^"]*)"$/
@@ -76,7 +79,162 @@ class SeWINiumContext extends RawSeWINiumContext implements TranslatableContext
        return;
     }
 
+     /**
+     * Select (add for later action) a widnow by title
+     * Example: Given I can select a window titled "winname"
+     *
+     * @Given /^(?:|I )can select a window titled "([^"]*)"$/
+     */
+    public function iCanSelectWindowTitle($title)
+    {
+        $ret=$this->Driver->apiSelectWindowByTitle($title);
+        if ($ret===false)
+        {
+            throw new \Exception("Window with title '".$title."' could not be selected. ".$this->Driver->getLastCommand());
+        }
 
+       return;
+    }
+
+    /**
+     * Find a window with title (Reg Exp)
+     * Example: Then I find a window titled like "<RegEx>"
+     *
+     * @Given /^(?:|I )find a window titled like "([^"]*)"$/
+     */
+    public function iCanFindWindowTitleLike($title)
+    {
+        $ret=$this->Driver->apiFindWindowLikeTitle($title);
+        if ($ret===false)
+        {
+            throw new \Exception("Window with title '".$title."' could not be selected. ".$this->Driver->getLastCommand());
+        }
+
+       return;
+    }
+
+    /**
+     * Select (add for later action) a window with title (Reg Exp)
+     * Example: Then I select a window titled like "<RegEx>"
+     *
+     * @Given /^(?:|I )select a window titled like "([^"]*)"$/
+     */
+    public function iCanSelectWindowTitleLike($title)
+    {
+        $ret=$this->Driver->apiSelectWindowLikeTitle($title);
+        if ($ret===false)
+        {
+            throw new \Exception("Window with title '".$title."' could not be selected. ".$this->Driver->getLastCommand());
+        }
+
+       return;
+    }
+
+ /* -------------------------------------- Window by class -----------------------------
+
+     /**
+     * Find a window with class
+     * Example: Given I can find a window with the class "class"
+     *
+     * @Given /^(?:|I )can find a window with the class "([^"]*)"$/
+     */
+    public function iCanFindWindowClass($title)
+    {
+        $ret=$this->Driver->apiFindWindowByClass($title);
+        if ($ret===false)
+        {
+            throw new \Exception("Window with class '".$title."' could not be found. ".$this->Driver->getLastCommand());
+        }
+
+       return;
+    }
+
+     /**
+     * Select (add for later action) a windows with the class
+     * Example: Given I can select a windows with the class "class"
+     *
+     * @Given /^(?:|I )can select a windows with the class "([^"]*)"$/
+     */
+    public function iCanSelectWindowClass($title)
+    {
+        $ret=$this->Driver->apiSelectWindowByClass($title);
+        if ($ret===false)
+        {
+            throw new \Exception("Window with class '".$title."' could not be selected. ".$this->Driver->getLastCommand());
+        }
+
+       return;
+    }
+
+    /**
+     * Find a window with class (Reg Exp)
+     * Example: Then I find a window with a class like "<RegEx>"
+     *
+     * @Given /^(?:|I )find a window with a class like "([^"]*)"$/
+     */
+    public function iCanFindWindowClassLike($title)
+    {
+        $ret=$this->Driver->apiFindWindowLikeClass($title);
+        if ($ret===false)
+        {
+            throw new \Exception("Window with class '".$title."' could not be selected. ".$this->Driver->getLastCommand());
+        }
+
+       return;
+    }
+
+    /**
+     * Select (add for later action) a window with class (Reg Exp)
+     * Example: Then I select a window with a class like "<RegEx>"
+     *
+     * @Given /^(?:|I )select a window with a class like "([^"]*)"$/
+     */
+    public function iCanSelectWindowClassLike($title)
+    {
+        $ret=$this->Driver->apiSelectWindowLikeClass($title);
+        if ($ret===false)
+        {
+            throw new \Exception("Window with class '".$title."' could not be selected. ".$this->Driver->getLastCommand());
+        }
+
+       return;
+    }
+
+/* -------------------------------------- Active Window -----------------------------
+
+     /**
+     * Find active window
+     * Example: Given I can find an active window
+     *
+     * @Given /^(?:|I )can find an active window
+     */
+    public function iCanFindActiveWindow()
+    {
+        $ret=$this->Driver->apiFindWindowActive();
+        if ($ret===false)
+        {
+            throw new \Exception("Active window could not be found. ".$this->Driver->getLastCommand());
+        }
+
+       return;
+    }
+
+    /**
+     * Select (add for later action)  active window
+     * Example: Given I can select the active window
+     *
+     * @Given /^(?:|I )can select the active window
+     */
+    public function iCanSelectActiveWindow()
+    {
+        $ret=$this->Driver->apiSelectWindowActive();
+        if ($ret===false)
+        {
+            throw new \Exception("Active window could not be selected. ".$this->Driver->getLastCommand());
+        }
+
+       return;
+    }
     
 
 //*******************************************************************************************
